887075374b3150a0ae78425e2378412d24db399e
Víctor <{GIT_EMAIL}>
2024-12-16 12:37:06 +0100

    refactor: :recycle: Updated object namespaces for Tests\Unit\Sql\Comando\Comando\Constructor\Delete

--- tests/Unit/Sql/Comando/Comando/ComandoDmlMock.php
+++ tests/Unit/Sql/Comando/Comando/ComandoDmlMock.php
@@ -7,6 +7,7 @@ namespace Tests\Unit\Sql\Comando\Comando;
 use Lib\Conexion\Conexion;
 use Lib\Sql\Comando\Clausula\ClausulaFabricaInterface;
 use Lib\Sql\Comando\Comando\ComandoDml;
+use Lib\Sql\Comando\Comando\DeleteComando;
 use Lib\Sql\Comando\Operador\Condicion\CondicionFabricaInterface;
 use Lib\Sql\Comando\Operador\GrupoOperadores;
 use Lib\Sql\Comando\Operador\TIPOS;
@@ -28,6 +29,24 @@ class ComandoDmlMock extends ComandoMockBase
     public function getComandoDmlMock(Conexion $conexion, ClausulaFabricaInterface $fabrica, CondicionFabricaInterface $fabrica_condiciones, array $metodos = []): ComandoDml&MockObject
     {
         return $this->getMockBuilder(ComandoDml::class)
+                    ->setConstructorArgs([$conexion, $fabrica, $fabrica_condiciones])
+                    ->onlyMethods($metodos)
+                    ->getMock();
+    }
+
+    /**
+     * Genera un mock de un comando.
+     *
+     * @version 1.0
+     *
+     * @param Conexion                  $conexion            conexión con la base de datos
+     * @param ClausulaFabricaInterface  $fabrica             Fabrica de clausulas SQL
+     * @param CondicionFabricaInterface $fabrica_condiciones Fábrica de condiciones
+     * @param list<non-empty-string>    $metodos             metodos para los que se crea un stub
+     */
+    public function getComandoDeleteMock(Conexion $conexion, ClausulaFabricaInterface $fabrica, CondicionFabricaInterface $fabrica_condiciones, array $metodos = []): DeleteComando&MockObject
+    {
+        return $this->getMockBuilder(DeleteComando::class)
                     ->setConstructorArgs([$conexion, $fabrica, $fabrica_condiciones])
                     ->onlyMethods($metodos)
                     ->getMock();
