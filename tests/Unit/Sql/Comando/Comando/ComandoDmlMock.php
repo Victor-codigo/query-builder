6d72ffef19d2dc1b0b909a8dd54d12bae1458bf2
Víctor <{GIT_EMAIL}>
2024-12-16 13:38:06 +0100

    refactor: :recycle: Updated object namespaces for Tests\Unit\Sql\Comando\Comando\Constructor\Select

--- tests/Unit/Sql/Comando/Comando/ComandoDmlMock.php
+++ tests/Unit/Sql/Comando/Comando/ComandoDmlMock.php
@@ -9,6 +9,7 @@ use Lib\Sql\Comando\Clausula\ClausulaFabricaInterface;
 use Lib\Sql\Comando\Comando\ComandoDml;
 use Lib\Sql\Comando\Comando\DeleteComando;
 use Lib\Sql\Comando\Comando\InsertComando;
+use Lib\Sql\Comando\Comando\SelectComando;
 use Lib\Sql\Comando\Operador\Condicion\CondicionFabricaInterface;
 use Lib\Sql\Comando\Operador\GrupoOperadores;
 use Lib\Sql\Comando\Operador\TIPOS;
@@ -66,6 +67,24 @@ class ComandoDmlMock extends ComandoMockBase
     public function getComandoInsertMock(Conexion $conexion, ClausulaFabricaInterface $fabrica, CondicionFabricaInterface $fabrica_condiciones, array $metodos = []): InsertComando&MockObject
     {
         return $this->getMockBuilder(InsertComando::class)
+                    ->setConstructorArgs([$conexion, $fabrica, $fabrica_condiciones])
+                    ->onlyMethods($metodos)
+                    ->getMock();
+    }
+
+    /**
+     * Genera un mock de un comando.
+     *
+     * @version 1.0
+     *
+     * @param Conexion                  $conexion            conexión con la base de datos
+     * @param ClausulaFabricaInterface  $fabrica             Fabrica de clausulas SQL
+     * @param CondicionFabricaInterface $fabrica_condiciones Fábrica de condiciones
+     * @param list<non-empty-string>    $metodos             metodos para los que se crea un stub
+     */
+    public function getComandoSelectMock(Conexion $conexion, ClausulaFabricaInterface $fabrica, CondicionFabricaInterface $fabrica_condiciones, array $metodos = []): SelectComando&MockObject
+    {
+        return $this->getMockBuilder(SelectComando::class)
                     ->setConstructorArgs([$conexion, $fabrica, $fabrica_condiciones])
                     ->onlyMethods($metodos)
                     ->getMock();
